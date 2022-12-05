e6f23d50aaf2e5e9b984cc5c4853e7729bfeafb3
MalKeMit <khodemalkemit@gmail.com>
2022-12-05 19:48:35 +0330

    namizun v1.2.3 - udp port set to popular game ports

--- namizun_core/udp.py
+++ namizun_core/udp.py
@@ -1,8 +1,8 @@
 from namizun_core import database
-
 from threading import Thread
 from random import randint
 from time import sleep
+from random import choice
 import socket
 
 
@@ -11,16 +11,22 @@ def get_random_ip():
     if fragmented_random_ip[0].isdigit() and 0 < int(fragmented_random_ip[0]) < 255 and \
             fragmented_random_ip[1].isdigit() and 0 < int(fragmented_random_ip[1]) < 255 and \
             fragmented_random_ip[2].isdigit() and 0 <= int(fragmented_random_ip[2]) < 255:
-        return f"{fragmented_random_ip[0]}.{fragmented_random_ip[1]}.{fragmented_random_ip[2]}.{randint(1, 255)}"
+        return f"{fragmented_random_ip[0]}.{fragmented_random_ip[1]}.{fragmented_random_ip[2]}." \
+               f"{fragmented_random_ip[3] if int(fragmented_random_ip[3]) != 0 else randint(1, 255)}"
     else:
         return get_random_ip()
 
 
+def get_game_port():
+    return choice([3478, 28960, 27014, 27020, 25565, 27015, 3724, 5000])
+
+
 def start_udp_uploader(udp_upload_size, target_ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    game_port = get_game_port()
     while udp_upload_size >= 0:
         buf = int(randint(3000, 5000) * database.get_cache_parameter('coefficient_buffer_size'))
-        if sock.sendto(bytes(buf), (target_ip, 443)):
+        if sock.sendto(bytes(buf), (target_ip, game_port)):
             udp_upload_size -= buf
             sleep(0.01)
     sock.close()
