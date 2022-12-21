0ad7dc3e088a3c3c6e904f008550fd1a8df47b50
MalKeMit <khodemalkemit@gmail.com>
2022-12-21 09:04:02 +0330

    namizun v1.3.7.1

--- namizun_core/log.py
+++ namizun_core/log.py
@@ -6,7 +6,7 @@ def store_restart_namizun_uploader_log():
     file = open('/var/www/namizun/udp.log', 'a')
     file.write(f"date: {get_now_date()} - "
                f"time: {get_now_time()} - "
-               f"namizun.service restarted")
+               f"namizun.service restarted\n")
     file.close()
 
 
@@ -16,7 +16,7 @@ def store_new_upload_loop_log(total_uploader_count, total_upload_size):
                f"time: {get_now_time()} - "
                f"new upload loop started - "
                f"[total uploader count: {total_uploader_count} - "
-               f"total upload size: {get_size(total_upload_size)}]")
+               f"total upload size: {get_size(total_upload_size)}]\n")
     file.close()
 
 
@@ -26,7 +26,7 @@ def store_new_upload_agent_log(uploader_count, upload_size_for_each_ip):
                f"time: {get_now_time()} - "
                f"new upload agent started - "
                f"[uploader count: {uploader_count} - "
-               f"upload size for each ip: {get_size(upload_size_for_each_ip)}]")
+               f"upload size for each ip: {get_size(upload_size_for_each_ip)}]\n")
     file.close()
 
 
@@ -38,5 +38,5 @@ def store_new_udp_uploader_log(started_at, target_ip, target_port, upload_size, 
                f"ip: {target_ip} - "
                f"port: {target_port} - "
                f"size: {get_size(upload_size)} - "
-               f"finish: {ended_at}]")
+               f"finish: {ended_at}]\n")
     file.close()
