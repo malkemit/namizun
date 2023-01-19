78a6c4053a68dfc4d2b6d1e611403e240a73f7cd
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2023-01-19 13:12:12 +0330

    Third Test

--- else/setup.sh
+++ else/setup.sh
@@ -37,7 +37,7 @@ if [ $? != 0 ]; then
 fi
 
 echo 'Installing project dependencies (step 5)'
-cd /var/www/namizun && source /var/www/namizun/venv/bin/activate && pip install wheel -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com && pip install namizun_core/ namizun_menu/ && deactivate
+cd /var/www/namizun && source /var/www/namizun/venv/bin/activate && pip install wheel -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com && pip install namizun_core/ namizun_menu/ -i https://pypi.tuna.tsinghua.edu.cn/simple/ && deactivate
 if [ $? != 0 ]; then
   echo "Dependencies doesn't installed correctly"
   exit
